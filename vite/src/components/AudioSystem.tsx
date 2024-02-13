c9583c967183a18e1b9b7039acbb693eeb2bb1cb
Damian Stewart <d@damianstewart.com>
2024-02-13 20:21:33 +1300

    wavesurfer regions wip

--- vite/src/components/AudioSystem.tsx
+++ vite/src/components/AudioSystem.tsx
@@ -11,6 +11,7 @@ import { useRef } from 'react'
 import GROOVE_MP3 from '../assets/groove.mp3'
 import WaveSurfer from "wavesurfer.js";
 import {debounce} from "../system/debounce.ts";
+import RegionsPlugin from "wavesurfer.js/dist/plugins/regions";
 
 const audiocraft = new Audiocraft(socket)
 
@@ -43,10 +44,75 @@ export function WavesurferPanel({url}: {url: string}) {
         height: 100
     })
 
+    //console.log("wavesurfer panel:", wavesurfer, isReady)
+
     const onPlayPause = () => {
         console.log('onPlayPause: wavesurfer is', wavesurfer)
         wavesurfer && wavesurfer.playPause()
     }
+
+    useEffect(() => {
+        console.log('wavesurfer', wavesurfer, 'isReady', isReady)
+
+        if (isReady) {
+            // Initialize the Regions plugin
+            const wsRegions = wavesurfer!.registerPlugin(RegionsPlugin.create())
+
+            // Create some regions at specific time ranges
+            // Give regions a random color when they are created
+            const random = (min, max) => Math.random() * (max - min) + min
+            const randomColor = () => `rgba(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)}, 0.5)`
+
+            // Regions
+            wsRegions.addRegion({
+                start: 0,
+                end: 0.8,
+                content: 'Resize me',
+                color: randomColor(),
+                drag: false,
+                resize: true,
+            })
+            wsRegions.addRegion({
+                start: 0.9,
+                end: 1,
+                content: 'Cramped region',
+                color: randomColor(),
+                minLength: 0.1,
+                maxLength: 1,
+            })
+            wsRegions.addRegion({
+                start: 1.2,
+                end: 1.7,
+                content: 'Drag me',
+                color: randomColor(),
+                resize: false,
+            })
+
+            // Markers (zero-length regions)
+            wsRegions.addRegion({
+                start: 1.9,
+                content: 'Marker',
+                color: randomColor(),
+            })
+            wsRegions.addRegion({
+                start: 2,
+                content: 'Second marker',
+                color: randomColor(),
+            })
+            wsRegions.enableDragSelection({
+                color: 'rgba(255, 0, 0, 0.1)',
+            })
+
+            wsRegions.on('region-updated', (region) => {
+                console.log('Updated region', region)
+            })
+
+
+        }
+
+    }, [wavesurfer, isReady])
+
+
 
     return <div>
         <div ref={containerRef} />
@@ -62,29 +128,45 @@ function cloneArray(items: any) {
 }
 
 
+class AudioRegion {
+    start: number = 0
+    end: number = 0
+    duration() { return this.end - this.start }
+}
 
+const MAX_DURATION = 1
 
 export function AudioSystem() {
 
     const [tokens, setTokens] = useState<number[][]>([[]])
-    const [usedAudio, setUsedAudio] = useState<ToneAudioBuffer|undefined>(undefined)
-    const [usedAudioBlobURL, setUsedAudioBlobURL] = useState<string|undefined>()
+    const [audioBuffer, setAudioBuffer] = useState<ToneAudioBuffer|undefined>(undefined)
+    const [range, setRange] = useState<AudioRegion|undefined>(undefined)
+    const [audioBlobURL, setAudioBlobURL] = useState<string|undefined>()
 
     useEffect(() => {
-        if (usedAudio) {
-            console.log("setting usedAudioBlobURL for", usedAudio)
-            const audioBlob = new Blob([audioBufferToWav(usedAudio.get()!)], {type: "audio/wav"})
-            setUsedAudioBlobURL(URL.createObjectURL(audioBlob))
+        if (audioBuffer) {
+            console.log("setting audioBlobURL for", audioBuffer)
+            const audioBlob = new Blob([audioBufferToWav(audioBuffer.get()!)], {type: "audio/wav"})
+            setAudioBlobURL(URL.createObjectURL(audioBlob))
         }
-    }, [usedAudio])
+    }, [audioBuffer])
+
+
+    useEffect(() => {
+        if (audioBuffer && range && range.duration() < MAX_DURATION) {
+            audiocraft.tokenize(audioBuffer!.slice(range.start, range.end)!, (tokens) => {
+                setTokens(tokens)
+            })
+        }
+    }, [audioBuffer, range])
+
 
     async function onAudioLoaded(buffer: ToneAudioBuffer) {
         console.log("onAudioLoaded, buffer is:", buffer)
-        const usedAudio = audiocraft.tokenize(buffer, (tokens) => {
-            setTokens(tokens)
-        })
-        setUsedAudio(usedAudio)
+        setRange(undefined)
+        setAudioBuffer(buffer.slice(0, 4))
     }
+
 
     function debouncedDetokenize(tokens: any, desiredSampleRate: number, callback: any) {
         debounce(() => {
@@ -106,7 +188,7 @@ export function AudioSystem() {
     return <div className="audiosystem">
         <AudioLoader loadedCallback={ onAudioLoaded }/>
         <TokensGrid data={tokens} tokensModifiedCallback={ onTokensModified } />
-        { usedAudioBlobURL && <WavesurferPanel url={usedAudioBlobURL} /> }
+        { audioBlobURL && <WavesurferPanel url={audioBlobURL} /> }
     </div>
 
 }
